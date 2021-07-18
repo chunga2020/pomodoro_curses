a44ff167a7724826c7b4f535416cfcf10f2ddc69
Andrew Chung <andrewtc4@gmail.com>
2021-07-18 12:22:36 -0400

    pomodoro_tests: Add null ptr test for Timer_set

--- tests/pomodoro_tests.c
+++ tests/pomodoro_tests.c
@@ -8,10 +8,21 @@ char *test_Timer_alloc() {
     return NULL;
 }
 
+char *test_Timer_set_null_ptr() {
+    Timer *t = NULL;
+    int rc = Timer_set(t, 0, 0, 0);
+    mu_assert(rc == -1,
+            "With a NULL Timer pointer, expected rc -1, got rc %d",
+            rc);
+
+    return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
     mu_run_test(test_Timer_alloc);
+    mu_run_test(test_Timer_set_null_ptr);
 
     return NULL;
 }
