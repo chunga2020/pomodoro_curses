4466d2925a8a7e0bd0349e1f23a9aeee2faf01d8
Andrew Chung <andrewtc4@gmail.com>
2021-07-18 14:38:52 -0400

    pomodoro_tests: Add negative sec. Timer_tick test

--- tests/pomodoro_tests.c
+++ tests/pomodoro_tests.c
@@ -169,6 +169,20 @@ char *test_Timer_tick_negative_minutes() {
     return NULL;
 }
 
+char *test_Timer_tick_negative_seconds() {
+    Timer *t = Timer_alloc();
+    mu_assert(t != NULL, "Timer_alloc failed.");
+    long int rc = Timer_set(t, 0, 0, 0);
+    t->seconds = -1;
+
+    rc = Timer_tick(t);
+    mu_assert(rc == -1, "With a negative second value, expected rc -1, got %ld",
+            rc);
+
+    Timer_destroy(t);
+    return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
@@ -186,6 +200,7 @@ char *all_tests() {
     mu_run_test(test_Timer_tick_null_ptr);
     mu_run_test(test_Timer_tick_negative_hours);
     mu_run_test(test_Timer_tick_negative_minutes);
+    mu_run_test(test_Timer_tick_negative_seconds);
 
     return NULL;
 }
