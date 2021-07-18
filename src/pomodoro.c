326522f30322d45a089571ce774fc52eb2d82433
Andrew Chung <andrewtc4@gmail.com>
2021-07-18 12:17:58 -0400

    pomodoro.c: Add Timer_set

--- src/pomodoro.c
+++ src/pomodoro.c
@@ -23,6 +23,46 @@ error:
     return NULL;
 }
 
+/*
+ * Sets a Timer to the specified time.
+ *
+ * Parameters:
+ *     hours: the number of hours to put on the timer; must not be negative
+ *     minutes: the number of minutes to put on the timer; must be in the 
+ *              interval [0, 59]
+ *     seconds: the number of seconds to put on the timer; must be in the
+ *              interval [0, 59]
+ * Returns:
+ *     0 on success
+ *     -1 on failure
+ */
+int Timer_set(Timer *t, int hours, int minutes, int seconds) {
+    check(t != NULL, "Got NULL Timer pointer.");
+    check(hours >= 0, "Cannot have negative 'hours' value '%d'.", hours);
+    check(0 <= minutes && minutes <= 59,
+            "Got bad 'minutes' value '%d'. Must be [0, 59]", minutes);
+    check(0 <= seconds && seconds <= 59,
+            "Got bad 'seconds' value '%d'. Must be [0, 59]", seconds);
+    
+    t->hours = hours;
+    check(t->hours == hours, "Hours not set correctly. Expected %d, got %d",
+            hours, t->hours);
+
+    t->minutes = minutes;
+    check(t->minutes == minutes,
+            "Minutes not set correctly. Expected %d, got %d", minutes,
+            t->minutes);
+
+    t->seconds = seconds;
+    check(t->seconds == seconds,
+            "Seconds not set correctly. Expected %d, got %d", seconds,
+            t->seconds);
+
+    return 0;
+error:
+    return -1;
+}
+
 /* 
  * Destroy a Timer object
  *
