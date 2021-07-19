bb0524c2adb48e4c14b60152403c308a6d749d2d
Andrew Chung <andrewtc4@gmail.com>
2021-07-19 14:27:22 -0400

    main: Add variables for cmd-line arguments

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <ncurses.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#include "dbg.h"
 #include "pomodoro.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    /* #### program options #### */
+    int opt; // variable for getting options with getopt(3)
+
+    // Default pomodoro (work session) length, in minutes
+    short int session_length = 25;
+
+    // Default short break length, in minutes
+    short int short_break_length = 5;
+
+    // Number of sessions before a long break
+    short int pomodoros_per_set = 3;
+
+    // Default long break length, in minutes
+    short int long_break_length = 30;
+
     int row = 0;
     int col = 0;
 
