09134fdcf1a9ecfd0d89fd615ca2db594dd7e590
Andrew Chung <andrewtc4@gmail.com>
2021-07-19 12:47:30 -0400

    minunit.h: Add __VA_ARGS__ to mu_assert
    Allows printed log messages to have variables

--- src/minunit.h
+++ src/minunit.h
@@ -8,8 +8,8 @@
 
 #define mu_suite_start() char *message = NULL
 
-#define mu_assert(test, message) if (!(test)) {\
-    log_err(message); return message; }
+#define mu_assert(test, message, ...) if (!(test)) {\
+    log_err(message, ##__VA_ARGS__); return message; }
 #define mu_run_test(test) debug("\n-----%s", " " #test);\
     message = test(); tests_run++; if (message) return message;
 
