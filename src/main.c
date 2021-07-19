8592df3611afeae4e1beaa3f9ecd6f181355e13d
Andrew Chung <andrewtc4@gmail.com>
2021-07-19 15:03:14 -0400

    main: Add set count option
    With default settings, one set is (25 min/session + 5 min short break)
    * 3 + (30 min long break) = 2 hours. This is a reasonable default, but
    if the user needs more sessions in a day, or uses short durations, it
    might be useful to be able to do multiple sets in one go.

--- src/main.c
+++ src/main.c
@@ -12,8 +12,8 @@ void usage(char *argv[]) {
     fprintf(stderr,
             "%s: A simple ncurses-based Pomodoro timer\n"
             "\n"
-            "Usage: %s [-h] [-b short_break_len] [-p sessions_per_set] "
-            "[-s session_length] [-B long_break_len]\n"
+            "Usage: %s [-h] [-b short_break_len] [-n sets] "
+            "[-p sessions_per_set] [-s session_length] [-B long_break_len]\n"
             "\n"
             "Note: all durations are in minutes\n"
             "\n"
@@ -21,6 +21,7 @@ void usage(char *argv[]) {
             "    -h\tShow this help message and exit\n"
             "\n"
             "    -b\tLength of breaks between work sessions (default 5)\n"
+            "    -n\tNumber of sets to work through (default 1)\n"
             "    -p\tNumber of pomodoros (work sessions) per set (default 3)\n"
             "    -s\tPomodoro session length (default 25)\n"
             "    -B\tLong break length (default 30)\n",
@@ -39,13 +40,16 @@ int main(int argc, char *argv[]) {
     // Default short break length, in minutes
     short int short_break_length = 5;
 
+    // Number of sets to work through
+    short int num_sets = 1;
+
     // Number of sessions before a long break
     short int pomodoros_per_set = 3;
 
     // Default long break length, in minutes
     short int long_break_length = 30;
 
-    while ((opt = getopt(argc, argv, "b:hp:s:B:")) != -1) {
+    while ((opt = getopt(argc, argv, "b:hn:p:s:B:")) != -1) {
         switch (opt) {
             case 'b':
                 short_break_length = atoi(optarg);
@@ -53,6 +57,9 @@ int main(int argc, char *argv[]) {
             case 'h':
                 usage(argv);
                 exit(EXIT_SUCCESS);
+            case 'n':
+                num_sets = atoi(optarg);
+                break;
             case 'p':
                 pomodoros_per_set = atoi(optarg);
                 break;
