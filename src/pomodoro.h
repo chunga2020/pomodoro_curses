e724a27cb32528ee8aa81242effa8422e3809fc9
Andrew Chung <andrewtc4@gmail.com>
2021-07-21 22:15:02 -0400

    pomodoro.{c,h}: Move doc comments to header
    Cleans up the implementation file, and acts as a guide if future implementations
    should be different.

--- src/pomodoro.h
+++ src/pomodoro.h
@@ -11,10 +11,51 @@ typedef struct {
     int seconds;
 } Timer;
 
+/*
+ * Allocates memory for a Timer object.
+ * 
+ * Parameters: none
+ * 
+ * Returns:
+ *     on success, a pointer to the new Timer
+ *     on failure, NULL
+ */
 Timer *Timer_alloc();
+
+/*
+ * Sets a Timer to the specified time.
+ *
+ * Parameters:
+ *     hours: the number of hours to put on the timer; must not be negative
+ *     minutes: the number of minutes to put on the timer; must be in the 
+ *              interval [0, 59]
+ *     seconds: the number of seconds to put on the timer; must be in the
+ *              interval [0, 59]
+ * Returns:
+ *     on success, 0
+ *     on failure, -1
+ */
 int Timer_set(Timer *t, int hours, int minutes, int seconds);
+
+/* 
+ * Destroy a Timer object
+ *
+ * Parameters:
+ *     t: the Timer to destroy
+ * Returns: none
+ */
 void Timer_destroy(Timer *t);
 
+/*
+ * Make a timer count down
+ *
+ * Parameters:
+ *     t: The timer to count down
+ * 
+ * Returns:
+ *     on success, the number of seconds remaining
+ *     on failure, -1
+ */
 int Timer_tick(Timer *t);
 
 #endif
